ed37d9b8351ae8b9ed9940c1657379e747327812
Ishan Kumarasinghe <e20211@eng.pdn.ac.lk>
2024-07-13 10:58:47 +0530

    Removing

--- Success/Success/demo/demo/src/main/java/management/example/demo/Repository/StudentRepository.java
+++
@@ -1,8 +0,0 @@
-package management.example.demo.Repository;
-
-
-import management.example.demo.Model.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudentRepository extends JpaRepository<Student, Long> {
-}
--- Success/Success/demo/src/main/java/management/example/demo/Repository/SignUpRepository.java
+++
@@ -1,8 +0,0 @@
-package management.example.demo.Repository;
-
-import management.example.demo.Model.SignUp;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SignUpRepository extends JpaRepository<SignUp, Long> {
-    SignUp findByRegNumber (String regNumber);
-}
