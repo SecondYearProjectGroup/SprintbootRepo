89b5c1c0aa8193408e1ccea3ccb4b3d408669e8d
dasunikawya2001 <150885245+dasunikawya2001@users.noreply.github.com>
2024-08-01 12:47:01 +0530

    Revert "upload"

--- demo_withLogginSuccess/demo_withLogginSuccess/src/main/java/management/example/demo/Service/FileUploadService.java
+++
@@ -1,7 +0,0 @@
-package management.example.demo.Service;
-
-import org.springframework.web.multipart.MultipartFile;
-
-public interface FileUploadService {
-    String uploadFile(MultipartFile file);
-}
