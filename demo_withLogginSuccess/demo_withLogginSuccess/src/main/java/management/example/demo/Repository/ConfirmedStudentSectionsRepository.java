64edf5e4b53354c4935ab74e392d21a3b3f516ec
dasunikawya2001 <e20197@eng.pdn.ac.lk>
2024-09-08 22:08:00 +0530

    Tabs

--- demo_withLogginSuccess/demo_withLogginSuccess/src/main/java/management/example/demo/Repository/ConfirmedStudentSectionsRepository.java
+++ demo_withLogginSuccess/demo_withLogginSuccess/src/main/java/management/example/demo/Repository/ConfirmedStudentSectionsRepository.java
@@ -7,4 +7,6 @@ import java.util.List;
 
 public interface ConfirmedStudentSectionsRepository extends JpaRepository<ConfirmedStudentSections, Long> {
     List<ConfirmedStudentSections> findByregNumber(String regNumber);
+
+    List<ConfirmedStudentSections> findByRegNumberAndActiveTab(String regNumber, String activeTab);
 }
