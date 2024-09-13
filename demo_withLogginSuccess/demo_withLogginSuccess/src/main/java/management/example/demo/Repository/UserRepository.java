1cd303f5b089a86eb87ccdbfa0c2f6b11cff90e7
dasunikawya2001 <e20197@eng.pdn.ac.lk>
2024-09-13 11:08:27 +0530

    User Profile Management

--- demo_withLogginSuccess/demo_withLogginSuccess/src/main/java/management/example/demo/Repository/UserRepository.java
+++ demo_withLogginSuccess/demo_withLogginSuccess/src/main/java/management/example/demo/Repository/UserRepository.java
@@ -2,6 +2,8 @@ package management.example.demo.Repository;
 
 import management.example.demo.Model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
@@ -11,4 +13,9 @@ public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findById(Long id);
 
     User save(User user);
+
+    @Query("SELECT u FROM User u WHERE u.id = :id")
+    Optional<User> findUserWithoutRolesById(@Param("id") Long id);
+
+
 }
--- demo_withLogginSuccess/demo_withLogginSuccess/src/main/java/management/example/demo/Service/UserService.java
+++ demo_withLogginSuccess/demo_withLogginSuccess/src/main/java/management/example/demo/Service/UserService.java
@@ -11,4 +11,5 @@ public interface UserService {
 
     User save(User user);
 
+
 }
